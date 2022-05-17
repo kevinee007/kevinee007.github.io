4c3269a18ded81bbf92647052eeeee89e05a67c3
unknown <kevinee007@gmail.com>
2022-05-18 01:56:46 +0800

    Updates

---
+++ precache-manifest.c1ace6f9d8bdbf62c098a762e9d358fc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7e465fe80444f217afad71790da561",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a48ee1843ecb6899437",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "240ffbb9d26c4dc73f22",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "cea6a75ad13a20a80c5e",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "ef1ca7e62561838ea48b",
+    "url": "/static/js/2.28792b26.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.28792b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a48ee1843ecb6899437",
+    "url": "/static/js/3.9ee8de3b.chunk.js"
+  },
+  {
+    "revision": "240ffbb9d26c4dc73f22",
+    "url": "/static/js/4.23c35949.chunk.js"
+  },
+  {
+    "revision": "cea6a75ad13a20a80c5e",
+    "url": "/static/js/main.214a694d.chunk.js"
+  },
+  {
+    "revision": "8c3b9cbae78bfa0256a6",
+    "url": "/static/js/runtime-main.516d248a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "cd3cfd8de80044751af815748a5e846e",
+    "url": "/static/media/victoriacollegeLogo.cd3cfd8d.png"
+  }
+]);
